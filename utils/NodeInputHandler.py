b7757460ea66a33de59c18c2e3dcc11b8e187719
bryantheball <bryanelijah.18@gmail.com>
2025-04-02 11:32:11 +0800

    feat(inputhandler): add input handling for ping

--- utils/NodeInputHandler.py
+++ utils/NodeInputHandler.py
@@ -1,11 +1,15 @@
+import time
+
+
 class NodeInputHandler:
     def __init__(self, node, spoof_flag, firewall_flag):
         self.node = node
         self.spoof_flag = spoof_flag
         self.firewall_flag = firewall_flag
         self.menu_actions = {
-            '1': {"msg": "Send a Message", "action":self.send_message},
-            '2': {"msg": "Exit Program", "action": self.exit_program}
+            '1': {"msg": "Ping", "action":self.ping},
+            '2': {"msg": "Send a Message", "action":self.send_message},
+            '3': {"msg": "Exit Program", "action": self.exit_program}
         }
         self.firewall_actions = {
             '1': "Add an IP rule",
@@ -15,9 +19,17 @@ class NodeInputHandler:
             '5': "Return to main menu"
         }
 
+        self.ping_actions = {
+            '1': 'Ping once',
+            '2': 'Ping on loop',
+            '3': 'Return to main menu'
+        }
+
+
         if self.firewall_flag:
-            self.menu_actions['3'] = self.menu_actions['2']
-            self.menu_actions['2'] = {"msg": "Manage Firewall Rules", "action": self.manage_firewall}
+            self.menu_actions['4'] = self.menu_actions['3']
+            self.menu_actions['3'] = {"msg": "Manage Firewall Rules", "action": self.manage_firewall}
+
         
     def run(self):
         while True:
@@ -32,8 +44,34 @@ class NodeInputHandler:
             else:
                 print("Invalid option. Please enter a valid number.")
 
+    def ping(self):
+        icmp_ip = input("Enter an IP to ping (e.g. 0x1A): ")
+        print("Enter the corresponding number:")
+        for key, value in self.ping_actions.items():
+            print(f"{key}. {value}")
+        ping_action = input("Your choice: ")
+        if ping_action in self.ping_actions:
+            if ping_action == "1":
+                self.node.send_icmp_request(icmp_ip)
+            elif ping_action == "2":
+                print("Press Ctrl+C to stop pinging.")
+                try:
+                    while True:
+                        print("Sending ping...")
+                        self.node.send_icmp_request(icmp_ip)
+                        time.sleep(2)
+                except KeyboardInterrupt:
+                    print("Stopped pinging.")
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+            else:
+                return
+        else:
+            print("Invalid option. Please enter a valid number.")
+
+
     def send_message(self):
-        dest = input("Enter destination MAC or IP: ").strip()
+        dest = input("Enter an IP to message: ").strip()
         msg = input("Enter message: ").strip()
         spoof_ip = ""
         if self.spoof_flag:
